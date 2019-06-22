09b48a53e843a7fad7ea15b40a7e9e886755cc2e
Fedor Chukharev <randomfedor@gmail.com>
2019-06-22 19:33:03 +0300

    Implement simple task list on index page

--- ennead/views/tasks.py
+++ ennead/views/tasks.py
@@ -1,7 +1,13 @@
 """Views for task pages"""
 from flask import render_template
 from werkzeug.wrappers import Response
+from ennead.models.task import TaskSet, Task
 
 
 def index() -> Response:
-    return render_template('index.html')
+    try:
+        active_set = TaskSet.get(TaskSet.active == 1)
+    except TaskSet.DoesNotExist:
+        return render_template('index.html', tasks=None)
+    tasks = Task.select().where(Task.task_set == active_set.active)
+    return render_template('index.html', tasks=tasks)
