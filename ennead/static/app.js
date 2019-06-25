3cf4e69960e7a9aa02fa3e64e2ca2ceaa45d96b3
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2019-06-25 22:52:25 +0300

    refine onload callback registration

--- ennead/static/app.js
+++ ennead/static/app.js
@@ -1,9 +1,13 @@
-window.onload = function(){
-  let textarea = document.getElementById('post-textarea');
-  textarea.addEventListener('keydown', function(event) {
-    if (event.ctrlKey && event.keyCode == 13) { // Ctrl-Enter pressed
-      let form = document.getElementById('post-form');
-      form.submit();
+window.addEventListener("load", function() {
+  let forms = document.getElementsByTagName('form');
+  for (let form of forms) {
+    let textareas = form.getElementsByTagName('textarea');
+    for (let textarea of textareas) {
+      textarea.addEventListener('keydown', function(event) {
+        if (event.ctrlKey && event.keyCode == 13) { // Ctrl-Enter pressed
+          form.submit();
+        }
+      });
     }
-  });
-};
+  }
+});
