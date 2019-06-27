fd43611bf15b97d3fbc84d21c035d8414984e6c7
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2019-06-27 10:13:18 +0300

    indentation tabs to 4 spaces

--- ennead/static/app.js
+++ ennead/static/app.js
@@ -1,13 +1,13 @@
 window.addEventListener("load", function() {
-  let forms = document.getElementsByTagName('form');
-  for (let form of forms) {
-    let textareas = form.getElementsByTagName('textarea');
-    for (let textarea of textareas) {
-      textarea.addEventListener('keydown', function(event) {
-        if (event.ctrlKey && event.keyCode == 13) { // Ctrl-Enter pressed
-          form.submit();
+    let forms = document.getElementsByTagName('form');
+    for (let form of forms) {
+        let textareas = form.getElementsByTagName('textarea');
+        for (let textarea of textareas) {
+            textarea.addEventListener('keydown', function(event) {
+                if (event.ctrlKey && event.keyCode == 13) { // Ctrl-Enter pressed
+                    form.submit();
+                }
+            });
         }
-      });
     }
-  }
 });
